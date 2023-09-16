dcc756485d61e0791d369781a4bfea296ff28358
StevePem <steve.pemberton@mail.com>
2023-09-16 14:56:09 +1000

    freight OD output for matsim simulation

--- freight.R
+++ freight.R
@@ -1,6 +1,13 @@
 # Using DTP freight volumes, allocate freight to links on an hourly basis
 
+# There are two versions, controlled by the 'matsimOutput' variable:
+# • matsim output - output is origin and destination addresses for trips, used 
+#   as an input for matsim to run the routing
+# • full output - this script runs the routing and calculates freight
+#   volumes for each link
+
 # Approach:
+# Introduction (both versions)-
 # • Process freight demand matrices to find the hourly volumes for each pair
 #   of traffic zones
 #   - Fractional demands are rounded; but if the rounded demands for the four
@@ -14,6 +21,17 @@
 #   meshblocks likely to generate freight traffic (categorised as 'commercial', 
 #   'industrial' or 'primary production')
 #   - For zones with no such addresses, the centroid is used as the 'address' 
+# For matsim output -
+# • For each trip, select random addresses from its origin and destination zones
+#   - If the same address is selected for both origin and destination, but another
+#     selection would be possible (because one or both has more than once possible
+#     address), then resample until the selected nodes are not the same
+#   - Trips that may have the same origin and destination address (for example, 
+#     because it is the centroid of the same zone) are not discarded (but will
+#     be unroutable)
+# • Find the X and Y coordinates of the relevant origin and destination addresses,
+#   and write as output.
+# For full output - 
 # • Find the nearest network node for each address
 #   - For zones where the centroid is used as the address, only nodes on 
 #     motorway, trunk or primary roads are used (to avoid exaggerated demand
@@ -31,7 +49,7 @@
 #   - Where multiple trips share a single pair of O/D nodes, the route is
 #     calculated only once, and the results joined to each trip
 # • Tally the number of trips using each link, for each hour; and sum the hourly
-#   totals for each link to produce daily totals
+#   totals for each link to produce daily totals; and write as output
 
 
 library(dplyr)
@@ -47,6 +65,8 @@ library(tidyr)  # pivoting
 dir_walk(path="./functions/",source, recurse=T, type = "file")
 
 PROJECT.CRS <- 28355
+
+matsimOutput <- T  # change to F if full output required
 
 
 # 1 Load freight, zones, meshblocks, addresses and network ----
@@ -138,7 +158,7 @@ freight.trips <- freight %>%
   ungroup()
 
 
-# 3 Allocate trip origin/destination locations ----
+# 3 Locate addresses ----
 # -----------------------------------------------------------------------------#
 # find addresses located in meshblocks for 'freight' purposes
 freight.meshblock.addresses <- addresses %>%
@@ -160,172 +180,194 @@ centroid.addresses <- zones %>%
   filter(TZN %in% zones.without.addresses) %>%
   st_centroid()
 
-# find nearest nodes to freight addresses 
-
-# find nearest nodes for addresses in freight meshblocks 
-# (note - st_nearest_feature finds index of nearest feature; then  index is 
-# used to find the id's of the relevant features) 
-n.node <- network.nodes$id[st_nearest_feature(freight.meshblock.addresses, network.nodes)]
-freight.meshblock.addresses <- cbind(freight.meshblock.addresses, n.node) 
-
-# find nearest nodes for zone centroids where no freight meshblock addresses - 
-# limited to major highways, to avoid exaggerating traffic on secondary or 
-# tertiary roads that may be near centroid
-major.network.links <- network.links %>%
-  filter(highway %in% c("motorway", "motorway_link", "trunk", "trunk_link",
-                        "primary", "primary_link"))
-major.network.nodes <- network.nodes %>%
-  filter(id %in% major.network.links$from_id | id %in% major.network.links$to_id)
-n.node <- major.network.nodes$id[st_nearest_feature(centroid.addresses, major.network.nodes)]
-centroid.addresses <- cbind(centroid.addresses, n.node) 
-
-# combine
-freight.addresses <- bind_rows(freight.meshblock.addresses,
-                               centroid.addresses)
-
-# write/reload (to avoid relocating nearest nodes) 
-# saveRDS(freight.addresses, "../data/processed/freight_addd.rds")
-# freight.addresses <- readRDS("../data/processed/freight_trips_nodes.rds")
-
-# write to sqlite for review 
-# st_write(freight.addresses, "../data/processed/freight_addresses.sqlite", 
-#          delete_dsn = TRUE)
-
-# randomly allocate origins and destinations to freight trips (about an hour)
-freight.trips.nodes <- allocateNodes(freight.trips, freight.addresses)
-# freight.trips.nodes <- allocateNodes(freight.trips[1:1000,], freight.addresses)
-
-# write/reload (to avoid need to reallocate random nodes) 
-# saveRDS(freight.trips.nodes, "../data/processed/freight_trips_nodes.rds")
-# freight.trips.nodes <- readRDS("../data/processed/freight_trips_nodes.rds")
-
-
-# 4 Find routes for each freight trip ----
+# 4 Produce required outputs
 # -----------------------------------------------------------------------------#
-# find unique OD pairs (to avoid calculating identical routes more than once)
-freight.trip.OD.pairs <- freight.trips.nodes %>%
-  distinct(orig.node, dest.node) %>%
-  # omit any where orig and dest nodes are the same
-  filter(orig.node != dest.node)
-
-# create the graph used for routing
-# prepare links and nodes (links need to be two-way; from_id and to_id to 
-# be first; requires weight field)
-g.links <- network.links %>%
-  st_drop_geometry() %>%
-  # weight: time
-  mutate(weight = length/freespeed) %>%
-  dplyr::select(from_id, to_id, id, is_oneway, weight)  # shortest time
-
-g.links.twoway.reversed <- g.links %>%
-  filter(is_oneway == 0) %>%
-  mutate(orig_from_id = from_id,
-         orig_to_id = to_id) %>%
-  mutate(from_id = orig_to_id,
-         to_id = orig_from_id) %>%
-  dplyr::select(names(g.links))
+if (outputMatsim) {
   
-g.links <- rbind(g.links, g.links.twoway.reversed) %>%
-  dplyr::select(-is_oneway)
+  ## 4.1 Matsim output ----
+  ## ------------------------------------#
+  freight.trip.ods <- 
+    allocateAddresses(freight.trips, freight.meshblock.addresses, centroid.addresses,
+                      freight.meshblock.address.TZNs)
+  
+  freight.trip.ods <- freight.trip.ods %>%
+    rename(orig_zn = Origin, dest_zn = Destination)
+  
+  write.csv(freight.trip.ods, "../data/processed/freight_trips_with_ODs.csv",
+            row.names = FALSE)
+  
+} else {
 
-g.nodes <- network.nodes %>%
-  st_drop_geometry()
-
-# create the graph
-g <- graph_from_data_frame(g.links, directed = T, vertices = g.nodes)
-
-
-# setup for parallel processing (using doSNOW instead of doParallel, because
-# allows progress reporting)
-cores <- detectCores()
-cluster <- parallel::makeCluster(cores)
-doSNOW::registerDoSNOW(cluster)
-
-# report
-print(paste(Sys.time(), "| Using parallel processing with", cores, 
-            "cores to find routes for", nrow(freight.trip.OD.pairs), 
-            "origin/destination pairs"))
-
-# set up progress reporting 
-pb <- txtProgressBar(max = nrow(freight.trip.OD.pairs), style = 3)
-progress <- function(n) setTxtProgressBar(pb, n)
-opts <- list(progress = progress)
-
-# find link id's for shortest route between each OD pair (about 10 hours)
-freight.trip.OD.pair.outputs <- 
-  foreach(i = 1:nrow(freight.trip.OD.pairs),
-  # foreach(i = 1:10,
-          .packages = c("dplyr", "sf", "igraph"), 
-          .combine = rbind,
-          # .verbose = TRUE,
-          .options.snow = opts) %dopar% {
-
-            # begin with output row, which is the current row of selected.OD.pairs                
-            output.row <- freight.trip.OD.pairs[i, ]
-            
-            # shortest route
-            shortest <- shortest_paths(g, 
-                                       from = as.character(output.row$orig.node),
-                                       to = as.character(output.row$dest.node),
-                                       mode = "out",
-                                       output = "epath")
-            
-            # complete details if shortest route exists, otherwise NA
-            if (length(shortest$epath[[1]]) > 0) {
-
-             link_ids <- edge_attr(g, "id", index = shortest$epath[[1]]) %>%
-               toString(.)
-
-            } else {
-              link_ids <- NA
-           }
-            
-            # add shortest route details to output row
-            output.row <- c(output.row,
-                            link_ids = link_ids)
-
-            # convert the output row (which has become a list) to a dataframe
-            output.row <- as.data.frame(output.row)
-            
-            # Return the output
-            return(output.row)
-            
-          }                                                    
-
-# end parallel processing
-close(pb)
-stopCluster(cluster)
-
-# result is a table of O/D pairs, with link IDs in a comma-separated string
-
-freight.trips.routes <- freight.trips.nodes %>%
-  left_join(., freight.trip.OD.pair.outputs, by = c("orig.node", "dest.node")) %>%
-  # remove any without links - either because orig and dest nodes were the same,
-  # or because no routes could be found
-  filter(!is.na(link_ids))
-
-# longer table, with one row per link for each trip's link ids
-freight.trips.routes.long <- freight.trips.routes %>%
-  mutate(link_ids = strsplit(link_ids, ",\\s*")) %>%
-  unnest(link_ids) %>%
-  pivot_longer(cols = link_ids, names_to = NULL, values_to = "link_id")
-
-
-# 5 Tally freight volumes for each link ----
-# -----------------------------------------------------------------------------#
-# group  by link and hour, and tally, add column for daily total
-link.volumes <- freight.trips.routes.long %>%
-  # tally number of trips per link and hour
-  group_by(link_id, hour) %>%
-  summarise(n = n()) %>%
-  ungroup() %>%
-  # set up hourly columns
-  arrange(hour) %>%
-  pivot_wider(names_from = hour, values_from = n, names_prefix = "hr_") %>%
-  # convert NAs to 0
-  mutate(across(starts_with("hr_"), ~ifelse(is.na(.), 0, .))) %>%
-  # add daily total
-  mutate(hr_total = rowSums(across(starts_with("hr_"), na.rm = TRUE)))
-
-write.csv(link.volumes, "../data/processed/freight_volumes.csv", row.names = FALSE)
+  ## 4.2 Full output ----
+  ## ------------------------------------#
+  ### 4.2.1  Find nearest nodes to freight addresses ----
+  ### -----------------------------------#
+  # find nearest nodes for addresses in freight meshblocks 
+  # (note - st_nearest_feature finds index of nearest feature; then  index is 
+  # used to find the id's of the relevant features) 
+  n.node <- network.nodes$id[st_nearest_feature(freight.meshblock.addresses, network.nodes)]
+  freight.meshblock.addresses <- cbind(freight.meshblock.addresses, n.node) 
+  
+  # find nearest nodes for zone centroids where no freight meshblock addresses - 
+  # limited to major highways, to avoid exaggerating traffic on secondary or 
+  # tertiary roads that may be near centroid
+  major.network.links <- network.links %>%
+    filter(highway %in% c("motorway", "motorway_link", "trunk", "trunk_link",
+                          "primary", "primary_link"))
+  major.network.nodes <- network.nodes %>%
+    filter(id %in% major.network.links$from_id | id %in% major.network.links$to_id)
+  n.node <- major.network.nodes$id[st_nearest_feature(centroid.addresses, major.network.nodes)]
+  centroid.addresses <- cbind(centroid.addresses, n.node) 
+  
+  # combine
+  freight.addresses <- bind_rows(freight.meshblock.addresses,
+                                 centroid.addresses)
+  
+  # write/reload (to avoid relocating nearest nodes) 
+  # saveRDS(freight.addresses, "../data/processed/freight_addd.rds")
+  # freight.addresses <- readRDS("../data/processed/freight_trips_nodes.rds")
+  
+  # write to sqlite for review 
+  # st_write(freight.addresses, "../data/processed/freight_addresses.sqlite", 
+  #          delete_dsn = TRUE)
+  
+  # randomly allocate origins and destinations to freight trips (about an hour)
+  freight.trips.nodes <- allocateNodes(freight.trips, freight.addresses)
+  # freight.trips.nodes <- allocateNodes(freight.trips[1:1000,], freight.addresses)
+  
+  # write/reload (to avoid need to reallocate random nodes) 
+  # saveRDS(freight.trips.nodes, "../data/processed/freight_trips_nodes.rds")
+  # freight.trips.nodes <- readRDS("../data/processed/freight_trips_nodes.rds")
+  
+  
+  ### 4.2.2 Find routes for each freight trip ----
+  ### ---------------------------------#
+  # find unique OD pairs (to avoid calculating identical routes more than once)
+  freight.trip.OD.pairs <- freight.trips.nodes %>%
+    distinct(orig.node, dest.node) %>%
+    # omit any where orig and dest nodes are the same
+    filter(orig.node != dest.node)
+  
+  # create the graph used for routing
+  # prepare links and nodes (links need to be two-way; from_id and to_id to 
+  # be first; requires weight field)
+  g.links <- network.links %>%
+    st_drop_geometry() %>%
+    # weight: time
+    mutate(weight = length/freespeed) %>%
+    dplyr::select(from_id, to_id, id, is_oneway, weight)  # shortest time
+  
+  g.links.twoway.reversed <- g.links %>%
+    filter(is_oneway == 0) %>%
+    mutate(orig_from_id = from_id,
+           orig_to_id = to_id) %>%
+    mutate(from_id = orig_to_id,
+           to_id = orig_from_id) %>%
+    dplyr::select(names(g.links))
+  
+  g.links <- rbind(g.links, g.links.twoway.reversed) %>%
+    dplyr::select(-is_oneway)
+  
+  g.nodes <- network.nodes %>%
+    st_drop_geometry()
+  
+  # create the graph
+  g <- graph_from_data_frame(g.links, directed = T, vertices = g.nodes)
+  
+  
+  # setup for parallel processing (using doSNOW instead of doParallel, because
+  # allows progress reporting)
+  cores <- detectCores()
+  cluster <- parallel::makeCluster(cores)
+  doSNOW::registerDoSNOW(cluster)
+  
+  # report
+  print(paste(Sys.time(), "| Using parallel processing with", cores, 
+              "cores to find routes for", nrow(freight.trip.OD.pairs), 
+              "origin/destination pairs"))
+  
+  # set up progress reporting 
+  pb <- txtProgressBar(max = nrow(freight.trip.OD.pairs), style = 3)
+  progress <- function(n) setTxtProgressBar(pb, n)
+  opts <- list(progress = progress)
+  
+  # find link id's for shortest route between each OD pair (about 10 hours)
+  freight.trip.OD.pair.outputs <- 
+    foreach(i = 1:nrow(freight.trip.OD.pairs),
+            # foreach(i = 1:10,
+            .packages = c("dplyr", "sf", "igraph"), 
+            .combine = rbind,
+            # .verbose = TRUE,
+            .options.snow = opts) %dopar% {
+              
+              # begin with output row, which is the current row of selected.OD.pairs                
+              output.row <- freight.trip.OD.pairs[i, ]
+              
+              # shortest route
+              shortest <- shortest_paths(g, 
+                                         from = as.character(output.row$orig.node),
+                                         to = as.character(output.row$dest.node),
+                                         mode = "out",
+                                         output = "epath")
+              
+              # complete details if shortest route exists, otherwise NA
+              if (length(shortest$epath[[1]]) > 0) {
+                
+                link_ids <- edge_attr(g, "id", index = shortest$epath[[1]]) %>%
+                  toString(.)
+                
+              } else {
+                link_ids <- NA
+              }
+              
+              # add shortest route details to output row
+              output.row <- c(output.row,
+                              link_ids = link_ids)
+              
+              # convert the output row (which has become a list) to a dataframe
+              output.row <- as.data.frame(output.row)
+              
+              # Return the output
+              return(output.row)
+              
+            }                                                    
+  
+  # end parallel processing
+  close(pb)
+  stopCluster(cluster)
+  
+  # result is a table of O/D pairs, with link IDs in a comma-separated string
+  
+  freight.trips.routes <- freight.trips.nodes %>%
+    left_join(., freight.trip.OD.pair.outputs, by = c("orig.node", "dest.node")) %>%
+    # remove any without links - either because orig and dest nodes were the same,
+    # or because no routes could be found
+    filter(!is.na(link_ids))
+  
+  # longer table, with one row per link for each trip's link ids
+  freight.trips.routes.long <- freight.trips.routes %>%
+    mutate(link_ids = strsplit(link_ids, ",\\s*")) %>%
+    unnest(link_ids) %>%
+    pivot_longer(cols = link_ids, names_to = NULL, values_to = "link_id")
+  
+  
+  ### 4.2.3 Tally freight volumes for each link ----
+  ### ---------------------------------#
+  # group  by link and hour, and tally, add column for daily total
+  link.volumes <- freight.trips.routes.long %>%
+    # tally number of trips per link and hour
+    group_by(link_id, hour) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    # set up hourly columns
+    arrange(hour) %>%
+    pivot_wider(names_from = hour, values_from = n, names_prefix = "hr_") %>%
+    # convert NAs to 0
+    mutate(across(starts_with("hr_"), ~ifelse(is.na(.), 0, .))) %>%
+    # add daily total
+    mutate(hr_total = rowSums(across(starts_with("hr_"), na.rm = TRUE)))
+  
+  write.csv(link.volumes, "../data/processed/freight_volumes.csv", row.names = FALSE)
+ 
+}
 
